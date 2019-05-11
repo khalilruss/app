7bd1b4fe207cdc07d5304d772ecb4102e0132902
khalil_russ <khalilruss@outlook.com>
2019-05-12 00:17:34 +0100

    annoying

--- app/src/main/java/com/kierigby/bountyhunter/NotificationsActivity.java
+++ app/src/main/java/com/kierigby/bountyhunter/NotificationsActivity.java
@@ -9,5 +9,5 @@ public class NotificationsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
-    }
+     }
 }

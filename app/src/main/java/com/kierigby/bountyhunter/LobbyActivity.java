42dd179c896d465a12a1e98496f75730a3c226fb
Azwad Rashid <azu.rashid1998@gmail.com>
2019-05-13 04:52:25 +0100

    Added Fragments to Lobby

--- app/src/main/java/com/kierigby/bountyhunter/LobbyActivity.java
+++ app/src/main/java/com/kierigby/bountyhunter/LobbyActivity.java
@@ -1,19 +1,38 @@
 package com.kierigby.bountyhunter;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LobbyActivity extends AppCompatActivity {
 
-    private TabLayout tabs;
+    private SectionPageAdapter sectionPageAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
-        tabs = findViewById(R.id.tabs);
+        sectionPageAdapter = new SectionPageAdapter(getSupportFragmentManager());
+
+        viewPager = findViewById(R.id.container);
+        setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+
+    public void setupViewPager(ViewPager viewPager) {
+        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new FragmentInLobbyActivity(), "In Lobby");
+        adapter.addFragment(new FragmentPendingActivity(), "Pending");
+        adapter.addFragment(new FragmentDeclinedActivity(), "Declined");
+        viewPager.setAdapter(adapter);
     }
 
 
